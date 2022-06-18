3bf179bb990292e708f35e41aab697741a674e8e
ediazjz <iam.ediazjz@gmail.com>
2022-06-18 10:53:34 -0500

    dependency: install Cloudinary integration
    Expanded the .env.example file to match current needs

--- config/middlewares.js
+++ config/middlewares.js
@@ -9,4 +9,18 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
